8b6d1c8c3973e5034b0b06bff979607b44f1bc1f
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-10-17 16:36:23 +0200

    add Carp component

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+	<article class="card">
+		<slot></slot>
+	</article>
+</template>
+
+<style scoped>
+
+	.card {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 1rem;
+		border: 1px solid var(--primary-10);
+		border-radius: 1rem;
+		background-color: var(--bg-secondary-40);
+		transition: all 0.3s ease;
+	}
+
+	.card:hover {
+		border: 1px solid var(--primary-30);
+	}
+
+</style>
