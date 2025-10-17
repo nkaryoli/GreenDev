c1ef2e3a47fff35db20a95e0f9c3e7bc54134691
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-10-18 01:34:38 +0200

    add sustenability section

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@ import Header from './components/Header.vue';
 import Hero from './components/Hero.vue';
 import Footer from './components/Footer.vue';
 import Services from './components/Services.vue';
+import Sustenability
+ from './components/Sustenability.vue';
 </script>
 
 <template>
   <Header/>
   <Hero />
   <Services />
+  <Sustenability />
   <Footer/>
 </template>
 
