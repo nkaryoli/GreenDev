0703a05c706783decbe08018180573f43d8eae10
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-10-25 20:48:09 +0200

    refactor

--- src/components/features/Contact/ContactInfomation.vue
+++ src/components/features/Contact/ContactInfomation.vue
@@ -1,44 +1,37 @@
 <script setup>
 import { MapPin, Phone, Mail } from 'lucide-vue-next';
+
+const items = [
+	{
+		icon: MapPin,
+		title: 'Oficina Principal',
+		description: 'Paseo de la Sostenibilidad 42 28001 Madrid, España'
+	},
+	{
+		icon: Phone,
+		title: 'Teléfono',
+		description: '+34 912 345 678'
+	},
+	{
+		icon: Mail,
+		title: 'Correo Electrónico',
+		description: 'Paseo de la Sostenibilidad 42	28001 Madrid, España'	
+	}
+]
 </script>
 
 <template>
 	<div class="section">
 		<h3>Información de Contacto</h3>
-		
-		<div class="contact-info">
+		<div class="contact-info" v-for="(item, index) in items" :key="index">
 			<div class="contact-icon">
-				<MapPin />
+				<component :is="item.icon" />
 			</div>
-			<div>
-				<h4>Oficina Principal</h4>
-				<p>Paseo de la Sostenibilidad 42
-					28001 Madrid, España</p>
+			<div class="contact-info-text">
+				<h4>{{ item.title }}</h4>
+				<p>{{ item.description }}</p>
 			</div>
 		</div>
-
-		<div class="contact-info">
-			<div class="contact-icon">
-				<Phone />
-			</div>
-			<div>
-				<h4>Oficina Principal</h4>
-				<p>Paseo de la Sostenibilidad 42
-					28001 Madrid, España</p>
-			</div>
-		</div>
-
-		<div class="contact-info">
-			<div class="contact-icon">
-				<Mail />
-			</div>
-			<div>
-				<h4>Oficina Principal</h4>
-				<p>Paseo de la Sostenibilidad 42
-					28001 Madrid, España</p>
-			</div>
-		</div>
-
 	</div>
 </template>
 
@@ -50,13 +43,24 @@ import { MapPin, Phone, Mail } from 'lucide-vue-next';
 .section h3 {
 	font-size: 1.5rem;
 	max-width: 800px;
-	margin-bottom: 3rem;
+	margin-top:  1rem;
+	margin-bottom: 2rem;
 }
 
 .contact-info {
 	display: flex;
 	gap: 1rem;
 	margin-bottom: 2rem;
+}
+
+.contact-info h4 {
+	font-size: 1rem;
+	margin-bottom: 0.5rem;
+}
+
+.contact-info p {
+	font-size: 0.8rem;
+	max-width: 25ch;
 }
 
 .contact-icon {
@@ -66,6 +70,8 @@ import { MapPin, Phone, Mail } from 'lucide-vue-next';
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	height: 60px;
+	width: 60px;
 }
 
 .contact-icon svg {
