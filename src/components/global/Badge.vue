ec34c5e4d6fb606dc2ecc5ea9e8e49a923a613bc
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-10-25 20:45:38 +0200

    add badge component

---
+++ src/components/global/Badge.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+	<span class="badge">
+		<slot></slot>
+	</span>
+</template>
+
+<style scoped>
+
+.badge {
+	display: inline-flex;
+	align-items: center;
+	gap: 0.5rem;
+	background-color: var(--primary-10);
+	padding: 0.4rem 1rem;
+	border-radius: 24px;
+	border: 1px solid var(--primary-30);
+	font-size: 0.85rem;
+	color: var(--primary-100);
+	margin-bottom: 2rem;
+	backdrop-filter: blur(10px);
+}
+
+</style>
