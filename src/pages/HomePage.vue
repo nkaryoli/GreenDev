a80cabdd3d207175afbedf613c90359c97f18638
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-10-26 01:33:07 +0200

    add sections to home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Hero from '../components/features/home/Hero.vue';
+import IDSection from '../components/features/home/IDSection.vue';
 import KmHomeSection from '../components/features/home/KmHomeSection.vue';
-import ODS from '../components/features/home/ODS.vue';
 import ServicesSection from '../components/features/home/ServicesSection.vue';
+import ODSSection from '../components/features/home/ODSSection.vue';
 import SustenabilitySection from '../components/features/home/SustenabilitySection.vue';
+import ContactSection from '../components/features/home/ContactSection.vue';
 </script>
 
 <template>
@@ -11,7 +13,9 @@ import SustenabilitySection from '../components/features/home/SustenabilitySecti
 	<ServicesSection />
 	<SustenabilitySection />
 	<KmHomeSection />
-	<ODS />
+	<IDSection/>
+	<ODSSection />
+	<ContactSection />
 </template>
 
 <style scoped>
